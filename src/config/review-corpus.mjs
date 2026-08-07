ecb7705ff87eb82595ba8c5171f6e95dd6dba94e
taekm33 <taejawow@gmail.com>
2026-08-07 13:16:42 +0900

    fix(adsense): constrain review corpus and remove template leakage

---
+++ src/config/review-corpus.mjs
@@ -0,0 +1,28 @@
+export const REVIEW_POSTS = [
+  "capsule-wardrobe-data",
+  "natsumae-aircon-shiunten-denkidai-checklist",
+  "natsu-genkan-kutsubako-nioi-shikke-reset-flow",
+  "genkan-bousai-bichiku-storage-small-home",
+  "natsu-furoba-kabi-yobou-kanki-mizukiri-flow",
+  "natsu-bentou-horeizai-mochiaruki-anzen-flow-2026",
+  "natsu-gohan-reikyaku-hozon-youki-eisei-flow-2026",
+  "natsu-horeibag-kaimono-eisei-shitaku-flow-2026",
+  "natsu-komebitsu-mushi-shikke-yobou-flow",
+  "natsu-makaita-houchou-kansou-shuunou-flow",
+  "natsu-mugicha-suitou-eisei-checklist",
+  "natsu-reizouko-nioi-ondo-reset-2026",
+  "natsu-seihyozara-kori-eisei-reizouko-flow-2026",
+  "natsu-senmenjo-taoru-shikke-eisei-flow-2026",
+  "natsu-setsuden-shanetsu-small-home",
+  "natsu-sponge-fukin-kansou-kokan-eisei-flow-2026",
+  "natsumae-amido-souji-mushi-yoke-kaze-flow-2026",
+  "natsumae-bousai-mizu-stock-mitsumori-checklist",
+  "natsumae-senpuki-circulator-souji-okikata-flow",
+  "reizouko-shouene-shuunou-food-loss",
+  "tsuyu-bathroom-kabi-nioi-reset-flow",
+  "tsuyu-genkan-kasa-kutsu-shikke-seiri-2026",
+  "tsuyu-heyaboshi-shikke-checklist",
+  "tsuyu-natsumae-futon-oshiire-shikke-flow"
+];
+export const REVIEW_POST_SET = new Set(REVIEW_POSTS);
+export const REVIEW_MIN_EQUIVALENT_WORDS = 850;
